011fcb7be764fa3f23a6715d586cc07db427ea80
Olubola-T <145150969+Olubola-T@users.noreply.github.com>
2025-01-18 14:41:54 +0100

    Update losses.py

--- functions/losses.py
+++ functions/losses.py
@@ -36,28 +36,6 @@ def noise_estimation_loss(model,
         return (e - output).square().sum(dim=(1, 2, 3)).mean(dim=0)
 
 
-
-def sr_noise_estimation_loss(model,
-                          x_bw: torch.Tensor,
-                          x_md: torch.Tensor,
-                          x_fw: torch.Tensor,
-                          t: torch.LongTensor,
-                          e: torch.Tensor,
-                          b: torch.Tensor, keepdim=False):
-    # a: a_T in DDIM
-    # 1-a: 1-a_T in DDIM 
-    a = (1-b).cumprod(dim=0).index_select(0, t).view(-1, 1, 1, 1)
-    # X_T
-    x = x_md * a.sqrt() + e * (1.0 - a).sqrt()
-
-    output = model(torch.cat([x_bw, x_fw, x], dim=1), t.float())
-    if keepdim:
-        return (e - output).square().sum(dim=(1, 2, 3))
-    else:
-        return (e - output).square().sum(dim=(1, 2, 3)).mean(dim=0)
-
-
-
 def sg_noise_estimation_loss(model,
                           x_img: torch.Tensor,
                           x_gt: torch.Tensor,
